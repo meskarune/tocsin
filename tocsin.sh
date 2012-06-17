efd8e5b80c11c585b3f18e4facde7c91567431b3
Dolores Portalatin <admin@doloresportalatin.info>
2012-06-17 15:07:59 -0500

    new file:   chimes/01-hour.mp3 	new file:   chimes/02-hour.mp3 	new file:   chimes/03-hour.mp3 	new file:   chimes/04-hour.mp3 	new file:   chimes/05-hour.mp3 	new file:   chimes/06-hour.mp3 	new file:   chimes/07-hour.mp3 	new file:   chimes/08-hour.mp3 	new file:   chimes/09-hour.mp3 	new file:   chimes/10-hour.mp3 	new file:   chimes/11-hour.mp3 	new file:   chimes/12-hour.mp3 	modified:   tocsin.sh

--- tocsin.sh
+++ tocsin.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
+#
+#Tocsin - Plays a Chime at the Hour and Quarter Hour
+
+# Quarter Chime Variables
 
 fifteen="./chimes/quarter1.mp3"
 thirty="./chimes/quarter2.mp3"
 fortyfive="./chimes/quarter3.mp3"
-hour="./chimes/quarter4.mp3"
-toll="./chimes/toll.mp3"
+
+# Hour Variable
+
+hour=`date +%I`
+
+# Hour Chime Variable
+
+hourchime="./chimes/$hour-hour.mp3"
+
+# Chime Functions
 
 chimeQ1 () {
-	mpg123 $(yes $fifteen | head -n $1)
+	mpg123 $(yes $hourchime | head -n $1)
 }
 chimeQ2 () {
-	mpg123 $(yes $thirty | head -n $1)
+	mpg123 $(yes $fifteen | head -n $1)
 }
 chimeQ3 () {
+	mpg123 $(yes $thirty | head -n $1)
+}
+chimeQ4 () {
 	mpg123 $(yes $fortyfive | head -n $1)
 }
 
-# Chime the Quarter Hours on the 15, 30, and 45 minute marks
+# Chime the Hour when minutes = 00 and Quarter Hours on the 15, 30, 
+# and 45 minute marks.
 
 case "$(date +%M)" in
-	15) 
+	00) 
 		chimeQ1
 		;;
-	30)
+	15) 
 		chimeQ2
 		;;
-	45)
+	30)
 		chimeQ3
+		;;
+	45)
+		chimeQ4
 		;;
 esac
